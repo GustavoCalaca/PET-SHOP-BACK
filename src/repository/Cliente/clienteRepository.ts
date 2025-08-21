d39ff48650d36288dcbf0d5363b16e2ff094ad17
Athos Bonner <athos.sti@hotmail.com>
2025-08-21 20:04:43 -0300

    Refatorando salvar endereço
    Refatorando salvar endereço

--- src/repository/Cliente/clienteRepository.ts
+++ src/repository/Cliente/clienteRepository.ts
@@ -8,29 +8,42 @@ export class ClienteRepository {
 
   static async salvar(cliente: ClienteDto): Promise<void> {
     //Salvando cliente 
-    const query = 'INSERT INTO clientes (nome, idade, cpf) VALUES ($1, $2, $3)';
+    const query = 'INSERT INTO clientes (nome, idade, cpf) VALUES ($1, $2, $3) RETURNING id';
     const values = [cliente.nome, cliente.idade, cliente.cpf];
-    //Salvando e pegando o id do cliente 
-    var idCliente = (await pool.query(query, values)).rows[0];
+    const result = await pool.query(query, values);
+    const idCliente = result.rows[0].id;
 
-    //Salvando endereco
+    console.log('Cliente salvo com sucesso:', cliente.nome);
+
+    // Salvando endereco
     const qualquerNome = `INSERT INTO endereco_cliente (logradouro, numero, bairro, cidade, complemento, estado, idcliente)
                           VALUES($1, $2, $3, $4, $5, $6, $7)`;
-    const vlEndereco = [cliente.endereco.logradouro, cliente.endereco.numero, cliente.endereco.bairro, cliente.endereco.cidade, cliente.endereco.complemeto, cliente.endereco.estado, idCliente];
+    const vlEndereco = [
+      cliente.endereco.logradouro,
+      cliente.endereco.numero,
+      cliente.endereco.bairro,
+      cliente.endereco.cidade,
+      cliente.endereco.complemeto,
+      cliente.endereco.estado,
+      idCliente
+    ];
+    
     await pool.query(qualquerNome, vlEndereco);
 
-    //Percorrendo a lista de contato
-    cliente.contato.forEach(async contato => {
-        //Para cada contato vindo do postman salvar no banco
-        const queryContato = `INSERT INTO contato_cliente (nome, telefone, email, idcliente)
-                              VALUES($1, $2, $3, $4)`;
-        const vlContato = [contato.nome, contato.telefone, contato.email, idCliente];
-        await pool.query(queryContato, vlContato);
+    console.log('Endereço salvo com sucesso:', cliente.endereco);
 
-    });
+//------ Fim de salvar endereço ------//
 
+    // Percorrendo a lista de contato
+    for (const contato of cliente.contato) {
+      const queryContato = `INSERT INTO contato_cliente (nome, telefone, email, idcliente)
+                            VALUES($1, $2, $3, $4)`;
+      const vlContato = [contato.nome, contato.telefone, contato.email, idCliente];
+      await pool.query(queryContato, vlContato);
+    }
 
-
+    //------ Fim de salvar contato ------//
+    console.log('Cliente salvo com sucesso:', cliente);
 
   }
 
