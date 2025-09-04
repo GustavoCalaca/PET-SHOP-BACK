d726865ccc1ffc4951036b31258f93db30364925
will costa <willams.cost@hotmail.com>
2025-09-03 21:59:07 -0300

    Atualização do Doença

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
 import express from 'express';
 import { ClienteController } from './controller/Cliente/clienteController';
-<<<<<<< HEAD
-import { addUsuario, listUsuario, updateUsuario, deleteUsuario } from "./controller/usuarioController";
-=======
-import { ProdutoController } from './controllers/DoencaController';
-import { ProdutoController } from './controllers/ProdutoController';
-import { ServicoController } from './controllers/ServicoController';
->>>>>>> 0de2baddaec9a758558aff137924421417e1e94d
+import { DoencaController } from './controller/Doenca/doencaController';
+
 
 const app = express();
 app.use(express.json());
@@ -17,27 +12,21 @@ app.post('/addcliente', ClienteController.addCliente);
 app.get('/listCliente', ClienteController.listarClientes);
 
 // Rotas
-app.post("/addusuario", addUsuario);
-app.get("/listusuario", listUsuario);
-app.put("/updateusuario/:id", updateUsuario);
-app.delete("/deleteusuario/:id", deleteUsuario);
-
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.post("/addusuario", UsuarioController.addUsuario);
+app.get("/listusuario", UsuarioController.listUsuario);
+app.put("/updateusuario/:id", UsuarioController.updateUsuario);
+app.delete("/deleteusuario/:id", UsuarioController.deleteUsuario);
 
 
 
 
-<<<<<<< HEAD
-=======
-
-app.post('/addDoenca', ClienteController.addDoenca); 
-app.get('/listDoenca', ClienteController.listarDoenca);
-app.delete('/deleteDoenca/:id', ClienteController.deletarDoenca);
-app.put('/updateDoenca/:id', ClienteController.atualizarDoenca); 
+app.post('/addDoenca', DoencaController.addDoenca); 
+app.get('/listDoenca', DoencaController.listarDoenca);
+app.delete('/deleteDoenca/:id', DoencaController.deletarDoenca);
+app.put('/updateDoenca/:id', DoencaController.atualizarDoenca); 
 
 
-app.post('/addProduto', ProdutoController.adicionarProduto);
+/*app.post('/addProduto', ProdutoController.adicionarProduto);
 app.get('/listProduto', ProdutoController.listarProdutos);
 app.delete('/deleteProduto/:id', ProdutoController.deletarProduto);
 app.put('/updateProduto/:id', ProdutoController.atualizarProduto);
@@ -47,13 +36,11 @@ app.put('/updateProduto/:id', ProdutoController.atualizarProduto);
 app.post('/addServico', ServicoController.adicionarServico);
 app.get('/listServico', ServicoController.listarServicos);
 app.delete('/deleteServico/:id', ServicoController.deletarServico);
-app.put('/updateServico/:id', ServicoController.atualizarServico);
+app.put('/updateServico/:id', ServicoController.atualizarServico);*/
 
  
 
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
-});
->>>>>>> 0de2baddaec9a758558aff137924421417e1e94d
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 
