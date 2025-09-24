3d5fc6ca87fbd70d9548f90e3296fb486c583f81
will costa <willams.cost@hotmail.com>
2025-09-24 17:32:02 -0300

    atualizado o usuário

--- src/index.ts
+++ src/index.ts
@@ -8,38 +8,29 @@ import { ProdutoController } from './controller/Produto/produtoController';
 const app = express();
 app.use(express.json());
 
-
 //CRUD Create, Read, Update, Delete 
 app.post('/addcliente', ClienteController.addCliente); 
 app.get('/listCliente', ClienteController.listarClientes);
-
 
 app.post('/addDoenca', DoencaController.addDoenca); 
 app.get('/listDoenca', DoencaController.listarDoenca);
 app.delete('/deleteDoenca/:id', DoencaController.deletarDoenca);
 app.put('/alterardoenca/:id', DoencaController.alterarDoenca);
 
-
-
-
 app.post('/addProduto', ProdutoController.addProduto);
 app.get('/listProduto', ProdutoController.listarProduto);
 app.delete('/deleteProduto/:id', ProdutoController.deletarProduto);
 app.put('/updateProduto/:id', ProdutoController.alterarProduto);
-
 
 app.post('/addServico', ServicoController.adicionarServico);
 app.get('/listServico', ServicoController.listarServicos);
 app.delete('/deleteServico/:id', ServicoController.deletarServico);
 app.put('/updateServico/:id', ServicoController.atualizarServico);
 
-
-
 app.post('/addusuario', UsuarioController.addUsuario);
 app.get('/listusuario', UsuarioController.listarUsuario);
 app.put('/updateusuario/:id', UsuarioController.atualizarUsuario);
 app.delete('/deleteusuario/:id', UsuarioController.deletarUsuario);
-
  
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
